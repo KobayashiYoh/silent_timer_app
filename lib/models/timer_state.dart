b90cb5fc1cbfde22fe6b6ebb17b0c507d476cc53
KobayashiYoh <y.kobayashi@4nonome.com>
2023-02-23 15:26:32 +0900

    1秒ずつカウントダウンしていく機能を追加

--- lib/models/timer_state.dart
+++ lib/models/timer_state.dart
@@ -6,7 +6,11 @@ part 'timer_state.freezed.dart';
 class TimerState with _$TimerState {
   const factory TimerState({
     required bool isActive,
+    required String timeText,
   }) = _TimerState;
 }
 
-const TimerState kInitialTimerState = TimerState(isActive: false);
+const TimerState kInitialTimerState = TimerState(
+  isActive: false,
+  timeText: '00:00:00',
+);
--- lib/providers/timer_notifier.dart
+++ lib/providers/timer_notifier.dart
@@ -8,8 +8,21 @@ final timerProvider = StateNotifierProvider<TimerNotifier, TimerState>((ref) {
 });
 
 class TimerNotifier extends StateNotifier<TimerState> {
-  TimerNotifier() : super(kInitialTimerState);
+  TimerNotifier() : super(kInitialTimerState) {}
+  int _hours = 10;
+  int _minutes = 10;
+  int _seconds = 10;
   Timer? _timer;
+
+  void subtractTime() {
+    int currentTotalSeconds = _hours * 3600 + _minutes * 60 + _seconds;
+    int newTotalSeconds = currentTotalSeconds - 1;
+    print('new total seconds: $newTotalSeconds');
+    _hours = (newTotalSeconds / 3600).floor();
+    _minutes = ((newTotalSeconds % 3600) / 60).floor();
+    _seconds = newTotalSeconds % 60;
+    print('$_hours:$_minutes:$_seconds');
+  }
 
   void onPressedPlayButton() {
     state.isActive ? _stopTimer() : _startTimer();
@@ -21,9 +34,7 @@ class TimerNotifier extends StateNotifier<TimerState> {
   void _startTimer() {
     _timer = Timer.periodic(
       const Duration(seconds: 1),
-      (timer) {
-        print('hoge');
-      },
+      (timer) => subtractTime(),
     );
   }
 
